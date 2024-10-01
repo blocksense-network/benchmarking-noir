775fdd89d1aa847e7c3b1dd2862bdfd37ba82007
Kamen Mladenov <kamen@syndamia.com>
2024-10-01 16:10:49 +0300

    feat(graph-coloring): Add implementation of a graph coloring Noir program

---
+++ graph-coloring/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A non-trivial ZKP";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { system = system; };
+
+    py-pkgs = pythonPackages: with pythonPackages; [
+      networkx
+    ];
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        (python3.withPackages py-pkgs)
+        gnumake
+      ];
+    };
+  };
+}
