38aea98c79d1241407f56f333b4af1613c90f8a3
Kamen Mladenov <kamen@syndamia.com>
2024-10-02 14:04:09 +0300

    feat(graph-coloring): Create adjacency matrix from prover

--- graph-coloring/generate-graph/__main__.py
+++ graph-coloring/generate-graph/__main__.py
@@ -1,20 +1,21 @@
 import networkx as nx
 import sys
+import scipy
+argv = sys.argv
 
-sys.stdout = open("Prover.toml", "wt")
+sys.stdout = open(argv[1], "wt")
 
 def to_toml_arr(x):
     return list(map(list, x))
 
-argv = sys.argv
-n = sys.argv[2] if len(argv) >= 2 else 7
-m = sys.argv[3] if len(argv) >= 3 else 9
+n = argv[2] if len(argv) > 2 else 100
+m = argv[3] if len(argv) > 3 else 1.5 * n
 
-G = nx.dense_gnm_random_graph(n, m)
+G = nx.gnm_random_graph(n, m)
 
-print("graph =", to_toml_arr(G.edges))
+print("graph =", to_toml_arr(nx.adjacency_matrix(G).todense()))
 
 coloring = list(nx.coloring.greedy_color(G).items())
 
 print("colors =", 1 + max(map(lambda x: x[1], coloring)))
-print("coloring =", to_toml_arr(coloring))
+print("coloring =", sorted(to_toml_arr(coloring)))
